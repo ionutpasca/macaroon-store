5d2f788f4ffd2acbcccb6b99c8030080f4b85d93
Pasca Ionut <pascaionut94@yahoo.com>
2017-04-26 00:02:06 +0300

    Added winston

---
+++ knex/migrations/20170425223114_chat_tables.js
@@ -0,0 +1,29 @@
+
+function up(knex, Promise) {
+	return Promise.all([
+		knex.schema
+			.createTable('chat_rooms', table => {
+				table.increments('id').primary();
+				table.integer('first_user').unsigned().notNullable().references('id').inTable('users');
+				table.integer('second_user').unsigned().notNullable().references('id').inTable('users');
+			}),
+		
+		knex.schema
+			.createTable('chat_messages', table => {
+				table.increments('id').primary();
+				table.integer('chat_room_id').unsigned().notNullable().references('id').inTable('chat_rooms');
+				table.integer('sender').unsigned().notNullable().references('id').inTable('users');
+				table.string('message', 255);
+				table.dateTime('sending_date');
+			})
+	]);
+};
+
+function down(knex, Promise) {
+	return Promise.all([
+
+	]);
+};
+
+module.exports.up = up;
+module.exports.down = down;
